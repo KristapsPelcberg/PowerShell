5e6e82da2a8a69bc603567e8dff20e9fb272cdc6
KristapsPelcberg <99206899+KristapsPelcberg@users.noreply.github.com>
2022-03-23 10:28:57 +0200

    Create process.ps1
    Add Start and Stop process examples

---
+++ StartProcess/process.ps1
@@ -0,0 +1,3 @@
+﻿Start-Process 'C:\windows\system32\notepad.exe'     #Start process from direct path
+Start-Sleep -s 5                                    #Stop script for 5 seconds
+Stop-Process -Name "notepad"                        #Close process
